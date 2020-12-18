876a7631191ab1c63c420173667e1269c96186f7
Michael Pellegrini <pellegrinimichael@yahoo.com>
2020-12-18 10:09:59 -0800

    Initial commit

---
+++ src/app/recipes-section/recipes-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipesSectionComponent } from './recipes-section.component';
+
+describe('RecipeComponent', () => {
+  let component: RecipesSectionComponent;
+  let fixture: ComponentFixture<RecipesSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecipesSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipesSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

a21c681000d8ff28d9c05b6e4a6f96b3e9e77e5f
Michael Pellegrini <mipelleg@microsoft.com>
2020-12-21 10:30:08 -0800

    Close each mysql connection

--- includes/get_recipes.php
+++ includes/get_recipes.php
@@ -3,9 +3,12 @@
 		include($_SERVER['DOCUMENT_ROOT'] . '/family-recipes/_config/db_connect.php');
 		$query = 'SELECT * FROM recipes ORDER BY Title';
 		$results = mysqli_query($link, $query);
+
 		while ($data = mysqli_fetch_assoc($results)) {
 			$recipes[] = $data;
 		}
+
+		mysqli_close($link);
 
 		return $recipes;
 	}
@@ -14,16 +17,23 @@
 		include($_SERVER['DOCUMENT_ROOT'] . '/family-recipes/_config/db_connect.php');
 		$query = "SELECT * FROM recipes WHERE Slug='$slug'";
 		$result = mysqli_query($link, $query);
-		return mysqli_fetch_row($result);
+		$row = mysqli_fetch_row($result);
+
+		mysqli_close($link);
+
+		return $row;
 	}
 
 	function search_recipes($term) {
 		include($_SERVER['DOCUMENT_ROOT'] . '/family-recipes/_config/db_connect.php');
 		$query = "SELECT * FROM recipes WHERE Title like '%$term%' ORDER BY Title";
 		$results = mysqli_query($link, $query);
+
 		while ($data = mysqli_fetch_assoc($results)) {
 			$recipes[] = $data;
 		}
+
+		mysqli_close($link);
 
 		return $recipes;
 	}
